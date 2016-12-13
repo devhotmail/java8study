b8184dbf2045c48ab90466cec83761847c540d28
gaolei\lsg <43575218@qq.com>
2016-12-13 09:42:59 +0800

    a1

---
+++ intelliplatform/src/lambdainaction/chap10/Apple.java
@@ -0,0 +1,34 @@
+package lambdainaction.chap10;
+
+/**
+ * Created by lsg on 2016/12/13.
+ */
+public class Apple {
+        String color;
+        int weight;
+
+    public Apple(String color, int weight) {
+        this.color = color;
+        this.weight = weight;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+    public int getWeight() {
+        return weight;
+    }
+
+    @Override
+    public String toString() {
+        return this.getColor();
+    }
+}
