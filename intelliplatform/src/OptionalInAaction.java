48ca4c561665586c11d47927fbf258227b5d5f6b
gaolei <43575218@qq.com>
2016-12-13 22:13:16 -0800

    delete intexxxform and create javainaction

--- intelliplatform/src/OptionalInAaction.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by lsg on 2016/12/12.
- */
-public class OptionalInAaction {
-    public static void main(String[] args) {
-
-    }
-}
--- intelliplatform/src/lambdainaction/chap10/Insurance.java
+++
@@ -1,9 +0,0 @@
-package lambdainaction.chap10;
-public class Insurance {
-
-    private String name;
-
-    public String getName() {
-        return name;
-    }
-}
